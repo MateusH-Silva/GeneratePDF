0fdf733e91a16eed5bf2b07c36c27dc884d40676
Mateus Silva <mateus.silva@bycoders.com.br>
2020-08-23 15:19:56 -0300

    add method export in controllers

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -1,31 +1,34 @@
 class AgreementsController < ApplicationController
-  before_action :set_agreement, only: [:show, :edit, :update, :destroy]
-
+  # No Before Action nós adicionamos também o export para que ele consiga pegar o agreement correto
+  before_action :set_agreement, only: [:show, :edit, :update, :destroy, :export]
+  # Nós incluimos aqui a lib que vamos criar chamada generate_pdf.rb
+  require './lib/generate_pdf'
+ 
   # GET /agreements
   # GET /agreements.json
   def index
     @agreements = Agreement.all
   end
-
+ 
   # GET /agreements/1
   # GET /agreements/1.json
   def show
   end
-
+ 
   # GET /agreements/new
   def new
     @agreement = Agreement.new
   end
-
+ 
   # GET /agreements/1/edit
   def edit
   end
-
+ 
   # POST /agreements
   # POST /agreements.json
   def create
     @agreement = Agreement.new(agreement_params)
-
+ 
     respond_to do |format|
       if @agreement.save
         format.html { redirect_to @agreement, notice: 'Agreement was successfully created.' }
@@ -36,7 +39,7 @@ class AgreementsController < ApplicationController
       end
     end
   end
-
+ 
   # PATCH/PUT /agreements/1
   # PATCH/PUT /agreements/1.json
   def update
@@ -50,7 +53,7 @@ class AgreementsController < ApplicationController
       end
     end
   end
-
+ 
   # DELETE /agreements/1
   # DELETE /agreements/1.json
   def destroy
@@ -60,14 +63,18 @@ class AgreementsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+ 
+  # Criamos este método que vai chamar nossa lib para gerar o PDF e depois redirecionar o user para o arquivo PDF
+  def export
+    GeneratePdf::agreement(@agreement.client_name, @agreement.description, @agreement.price)
+    redirect_to '/agreement.pdf'
+  end
+ 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_agreement
-      @agreement = Agreement.find(params[:id])
+      @agreement = Agreement.find(params['id'])
     end
-
-    # Only allow a list of trusted parameters through.
+ 
     def agreement_params
       params.require(:agreement).permit(:client_name, :description, :price)
     end
